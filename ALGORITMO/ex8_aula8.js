13f26ec675c25f4973b153083a9f76baa5db7c7f
andreflavio <141851943+andreflavio@users.noreply.github.com>
2023-08-30 20:25:42 -0300

    Add files via upload

--- ALGORITMO/ex8_aula8.js
+++ ALGORITMO/ex8_aula8.js
@@ -4,14 +4,16 @@ let classificacao = "";
 
 if (isNaN(temperatura)) {
   classificacao = "Por favor, insira um valor numérico válido.";
-} else if (temperatura < 35.0) {
+} else if (temperatura <= 35.0) {
   classificacao = "Hipotermia";
-} else if (temperatura >= 35.0 && temperatura < 37.5) {
+} else if (temperatura >= 35.0 && temperatura < 37.7) {
   classificacao = "Normal";
-} else if (temperatura >= 37.5 && temperatura < 38.0) {
+} else if (temperatura >= 37.8 && temperatura < 39.5) {
   classificacao = "Febre";
-} else {
+} else if (temperatura >= 39.6 && temperatura < 41) {
   classificacao = "Febre Alta";
+} else if (temperatura >= 41) {
+  classificacao = "Hipertemia";
 }
-
+  
 console.log(`A classificação da temperatura é: ${classificacao}`);
